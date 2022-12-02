9e1cb71936db48d0553162c77eb4ee3f03dd9b8d
Zhdana <zhdanasulina@yandex.ru>
2022-12-02 15:11:56 +0300

    first commit

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA>numberB)
+{
+Console.Write("Больше:");
+Console.WriteLine(numberA);
+}
+else
+{
+Console.Write("Больше:");
+Console.WriteLine(numberB);
+}
